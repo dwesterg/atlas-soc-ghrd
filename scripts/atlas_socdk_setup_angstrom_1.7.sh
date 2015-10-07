d940d4550c8ad10023ba45c3192abb6f8813fe94
Dalon Westergreen <dwesterg@gmail.com>
2015-10-07 10:57:52 -0700

    Update Angstrom setup / build script

--- scripts/atlas_socdk_setup_angstrom_1.7.sh
+++ scripts/atlas_socdk_setup_angstrom_1.7.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 # Clone 1.7 angstrom repo
-git clone -b angstrom-v2014.12-yocto1.7 git://github.com/Angstrom-distribution/setup-scripts.git
+git clone git://github.com/Angstrom-distribution/setup-scripts.git
 
 # Add sources to layers.txt
 echo "meta-altera,git://github.com/dwesterg/meta-altera.git,angstrom-v2014.12-yocto1.7,HEAD" >> setup-scripts/sources/layers.txt
 echo "meta-altera-refdes,git://github.com/dwesterg/meta-altera-refdes.git,angstrom-v2014.12-yocto1.7,HEAD" >> setup-scripts/sources/layers.txt
+
+#sed -i 's/meta-linaro.git.*/meta-linaro,git:\/\/git.linaro.org\/openembedded\/meta-linaro.git,master,HEAD/' setup-scripts/sources/layers.txt
+
 
 # add BBLayers
 sed -i '/meta-96boards/a \ \ \$\{TOPDIR\}\/sources\/meta-altera-refdes \\' setup-scripts/conf/bblayers.conf
 sed -i '/meta-96boards/a \ \ \$\{TOPDIR\}\/sources\/meta-altera \\' setup-scripts/conf/bblayers.conf
 
 # disable kde4 repo  / gitorious gone
-sed -i '/meta-kde4/d' setup-scripts/sources/layers.txt
-sed -i '/meta-kde4/d' setup-scripts/conf/bblayers.conf
+#sed -i '/meta-kde4/d' setup-scripts/sources/layers.txt
+#sed -i '/meta-kde4/d' setup-scripts/conf/bblayers.conf
 
 # gumstix on gitorious too
 
-sed -i '/meta-gumstix-community/d' setup-scripts/sources/layers.txt
-sed -i '/meta-gumstix-community/d' setup-scripts/conf/bblayers.conf
+#sed -i '/meta-gumstix-community/d' setup-scripts/sources/layers.txt
+#sed -i '/meta-gumstix-community/d' setup-scripts/conf/bblayers.conf
 
 sed -i '/rm_work/d' setup-scripts/conf/local.conf
 
@@ -32,8 +35,14 @@ sed -i '/SSTATE_DIR/d' setup-scripts/conf/site.conf
 echo "DL_DIR = \"\${TOPDIR}/../downloads\"" >> setup-scripts/conf/site.conf
 echo "SSTATE_DIR = \"\${TOPDIR}/../ang_sstate_cache\"" >> setup-scripts/conf/site.conf
 
+# this qemu doesnt build with gcc5
+rm setup-scripts/sources/meta-linaro/meta-linaro/recipes-devtools/qemu/qemu_git.bb
+
 cd setup-scripts
 source environment-angstrom-v2014.12
+
+#mkdir sources/meta-altera-refdes/recipes-kernel/gator
+#cp ../gator_git.bbappend sources/meta-altera-refdes/recipes-kernel/gator
 
 export KERNEL_PROVIDER="linux-altera"
 export BB_ENV_EXTRAWHITE="${BB_ENV_EXTRAWHITE} KERNEL_PROVIDER"
